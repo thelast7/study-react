4f6d438cd93fdd069c6f51ac22ec33f956c54c09
thelast7 <takimanwar@gmail.com>
2019-01-03 21:36:22 +0700

    component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Greet } from './Component/greet';
-import Welcome from './Component/welcome';
+import { Greet } from './Component/Greet';
+import Welcome from './Component/Welcome';
+import Hello from './Component/Hello';
 
 class App extends Component {
   render() {
@@ -24,6 +25,7 @@ class App extends Component {
         </header> */}
         <Greet />
         <Welcome />
+        <Hello />
       </div>
     );
   }
---
+++ src/Component/Hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hello = () => {
+    return (
+        <div>
+            ini file jsx
+        </div>
+    )
+}
+
+export default Hello
